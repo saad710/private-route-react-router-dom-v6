b3b1fa833d56a4b364b7e70f8b7ef56e27ec6149
saad710 <ifrathasnat2@gmail.com>
2022-03-12 17:17:43 +0600

    nested-route-in component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" exact element={<Navigate to='/hello' />}/>
-        <Route path="/hello" element={<Hello />} />
+        <Route path="/hello/*" element={<Hello />} />
         <Route path="/posts" exact element={ <Posts />} />
         <Route path="/posts/:postId" element={ <Post />} />
       </Routes>
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,12 +1,12 @@
-import { Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 export default function Hello() {
   return (
     <div>
       <h1>Hello</h1>
       <p>This is the home page</p>
-      {/* <Route path="/hello/world">
-        <p>This is world!</p>
-      </Route> */}
+     <Routes>
+        <Route path="world" element={<p>this is world</p>} />
+     </Routes>
     </div>
   );
 }
