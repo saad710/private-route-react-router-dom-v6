263bab1307fdc6ed1fe55f01d6d7d3a68134db91
saad710 <ifrathasnat2@gmail.com>
2022-03-12 17:38:14 +0600

    useNavigate for back

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,17 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 export default function Post() {
   const params = useParams();
+  const navigate = useNavigate();
+  
+  const handleGoBack = () => {
+      navigate('/posts')
+  }
 
   return (
     <div>
       <h1>Post</h1>
       <p>Post id is {params.postId}</p>
+      <button type="button" onClick={handleGoBack}> Go to posts </button>
     </div>
   );
 }
