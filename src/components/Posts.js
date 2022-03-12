e4008cd88c706e4049a953704081ed63469d5d78
saad710 <ifrathasnat2@gmail.com>
2022-03-12 17:31:32 +0600

    use-params

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,19 +1,26 @@
 import { Link } from "react-router-dom";
 
+
+const posts = [
+  {name : "Post One", id:"1"},
+  {name : "Post Two", id:"2"},
+  {name : "Post Three", id:"3"},
+]
+
 export default function Posts() {
   return (
     <div>
       <h1>Posts</h1>
       <ul>
-        <li>
-          <Link to="/posts/1">Post 1</Link>
-        </li>
-        <li>
-          <Link to="/posts/2">Post 2</Link>
-        </li>
-        <li>
-          <Link to="/posts/3">Post 3</Link>
-        </li>
+        {
+          posts.map((data, index) => (
+          <li key={index}>
+            <Link to={`/posts/${data.id}`}>{data.name}</Link>
+          </li>
+          ))
+        }
+       
+        
       </ul>
     </div>
   );
